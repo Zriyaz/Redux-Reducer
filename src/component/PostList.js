6623246884756169483d9de387b00486b547f91c
Md Riyaz Kazmi <mdriyazansari.18mca@kongu.edu>
2020-05-19 06:17:49 +0000

    Initial commit

---
+++ src/component/PostList.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+class PostList extends React.Component{
+  render(){
+    return(
+      <div>
+       <h1>User Post:</h1>
+       </div>
+    )
+  }
+}
+export default PostList
