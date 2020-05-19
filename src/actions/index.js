45a9da41deb5b42b15e29bb8b8bc5ea9cba95373
Md Riyaz Kazmi <mdriyazansari.18mca@kongu.edu>
2020-05-19 14:20:00 +0000

    //commit

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+import jsonPlaceholder from "../apis/jsonPlaceholder"
+export const fetchPost = ()=>{
+  return function(){
+    const promise  =  jsonPlaceholder.get('/posts')
+  return{
+    type:'FETCH_POST',
+    payload:promise
+  }
+  }
+}
+
--- src/component/PostList.js
+++ src/component/PostList.js
@@ -1,6 +1,10 @@
 import React from "react"
-
+import {connect} from "react-redux"
+import {fetchPost} from "../actions"
 class PostList extends React.Component{
+  componentDidMount(){
+    this.props.fetchPost()
+  }
   render(){
     return(
       <div>
@@ -9,4 +13,7 @@ class PostList extends React.Component{
     )
   }
 }
-export default PostList
+export default connect(
+  null,
+  {fetchPost}
+  )(PostList )
